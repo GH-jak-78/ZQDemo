89beab189db662d373ff551e49ff206e2eda381b
jak_78 <jak_78@sina.com>
2016-09-07 11:39:21 +0800

    Demo初始化

---
+++ ZQDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZQDemo
+//
+//  Created by ZhaoQu on 16/9/7.
+//  Copyright © 2016年 ZQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZQDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZQDemo
+//
+//  Created by ZhaoQu on 16/9/7.
+//  Copyright © 2016年 ZQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
